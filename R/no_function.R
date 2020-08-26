d2dcc518d7847b7e5731aa52e6191cbf98499424
sdhaliwal7 <67290557+sdhaliwal7@users.noreply.github.com>
2020-08-26 10:54:48 -0700

    No functions

--- R/no_function.R
+++ R/no_function.R
@@ -13,14 +13,7 @@
 #' @import purrr
 #'
 #' @export
-sing_day <- function(dataset, line, phrase_col, num_word){
-    phrases <- dataset %>% pull({{phrase_col}})
-    num_word <- dataset %>% pull({{num_word}})
-    word <- num_word[line+1]
-    line = line
-    full <- paste("On the", word, "day of Christmas, my true love sent to me,", phrases, "and a partridge in a pear tree.")
-    return(full)
-    }
+
 
 
 
